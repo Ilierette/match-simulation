a6e90bbdc75a51eda3cbc6a8e6e416d82de85c74
Karolina Pietraś <Ilierette@users.noreply.github.com>
2022-07-04 13:36:50 +0200

    css timer

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,6 +2,7 @@ import { TeamRowContainer } from "container/teamRow";
 import React, { useEffect, useState } from "react";
 
 export interface TeamProps {
+    id: number
     teamOne: string
     teamTwo: string
     teamOneResult: number
@@ -10,30 +11,21 @@ export interface TeamProps {
 
 const data = [
     {
-        id: 0,
         teamOne: "Germany",
-        teamOneResult: 0,
         teamTwo: "Poland",
-        teamTwoResult: 0,
     },
     {
-        id: 1,
         teamOne: "Brazil",
-        teamOneResult: 0,
         teamTwo: "Mexico",
-        teamTwoResult: 0,
     },
     {
-        id: 2,
         teamOne: "Argentina",
-        teamOneResult: 0,
         teamTwo: "Uruguay",
-        teamTwoResult: 0,
     }
 ]
 
 export const MainPage = () => {
-    const [teams, setTeams] = useState(data);
+    const [teams, setTeams] = useState([]);
     const [time, setTime] = useState(null);
     const [restart, setRestart] = useState(false);
     const [goals, setGoals] = useState(0)
@@ -42,13 +34,25 @@ export const MainPage = () => {
     let interval = React.useRef<any>();;
 
     useEffect((() => {
-        setTeams(data)
+        handleReset()
     }), [])
+
+    const handleReset = () => {
+        const temp = data.map((i, index) => {
+            return ({
+                ...i,
+                id: index,
+                teamOneResult: 0,
+                teamTwoResult: 0
+            })
+        })
+        setTeams(temp)
+    }
 
 
     const handleTimer = () => {
         setRestart(false)
-        setTeams(data)
+        handleReset()
         startTime = Date.now();
         interval.current = setInterval(function () {
             setTime(((Date.now() - startTime) / 1000).toFixed(0));
@@ -65,10 +69,10 @@ export const MainPage = () => {
                 let random = handleRandom(teams)
                 const rand = (Math.floor(Math.random() * 100))
                 if (rand >= 50) {
-                    const temp = teams.map((team) => { if (team.id === random.id) { return ({ ...team, teamOneResult: team.teamOneResult + 1 }) } else return team })
+                    const temp = teams.map((team: TeamProps) => { if (team.id === random.id) { return ({ ...team, teamOneResult: team.teamOneResult + 1 }) } else return team })
                     setTeams(temp)
                 } else {
-                    const temp = teams.map((team) => { if (team.id === random.id) { return ({ ...team, teamTwoResult: team.teamTwoResult + 1 }) } else return team })
+                    const temp = teams.map((team: TeamProps) => { if (team.id === random.id) { return ({ ...team, teamTwoResult: team.teamTwoResult + 1 }) } else return team })
                     setTeams(temp)
                 }
             }
@@ -83,22 +87,34 @@ export const MainPage = () => {
     }
 
     useEffect((() => {
-        const temp = teams.map((team) => { return team.teamOneResult + team.teamTwoResult }).reduce((sum, i) => { return sum + i })
-        setGoals(temp)
+        if (teams.length) {
+            const temp = teams.map((team) => { return team.teamOneResult + team.teamTwoResult }).reduce((sum, i) => { return sum + i })
+            setGoals(temp)
+        }
     }), [teams])
 
 
     return (
         <div className="container">
+            <div className="base-timer">
+                <svg className="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                    <g className="base-timer__circle">
+                        <circle className="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+                    </g>
+                </svg>
+                <span className="base-timer__label">
+                    {time ? time : <>0</>}
+                </span>
+            </div>
+
             {
                 time ? restart ? <button className="btn" onClick={handleTimer}>restart</button> : <button className="btn" onClick={stopTimer}>przerwij</button> :
                     <button className="btn" onClick={handleTimer}>start</button>
             }
 
-            {time}
             <div className="team-container">
                 {
-                    teams.map((team: TeamProps) => (
+                    teams && teams.map((team: TeamProps) => (
                         <TeamRowContainer team={team} />
                     ))
                 }
